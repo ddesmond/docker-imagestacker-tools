172961451777a6b83ef8de6a634b7b1229bd8203
Aleks <crni.aki@gmail.com>
2025-07-14 09:05:27 +0200

    init

--- setup/reset_license.sh
+++ setup/reset_license.sh
@@ -3,23 +3,26 @@ cd /opt/ImageStacker_1.3.0_Linux_x86/ImageStacker/
 
 # curl https://emildohne.com/
 
-./ImageStackerCLI --help
+# ./ImageStackerCLI --help
 
 echo " ------------------------------------------- "
 echo "IS_LICENSE_KEY" $IS_LICENSE_KEY
 echo " ------------------------------------------- "
-./ImageStackerCLI --activate-offline $IS_LICENSE_KEY
+
+# cleanup all old lic files
+rm ImageStacker_*.lic
+
+# deactivate old license
+./ImageStackerCLI --deactivate $IS_LICENSE_KEY
 
 
 # activate the license offline and pickup the path
+./ImageStackerCLI --activate-offline $IS_LICENSE_KEY
 
-
-
-echo " ------------------------------------------- "
-echo "IS_LICENSE_KEY" $IS_LICENSE_KEY
-echo " ------------------------------------------- "
 
 
 # copy license file back to network shares
 
-cp *.lic /outputs/
+cp ImageStacker_*.lic /outputs/
+
+ls -la /outputs
