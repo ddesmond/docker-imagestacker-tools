00654eb1c60273bc744985ca713b9d1a5fef26d0
Aleks <crni.aki@gmail.com>
2025-07-14 08:29:06 +0200

    init

--- setup/setup_imagestacker.sh
+++ setup/setup_imagestacker.sh
@@ -7,3 +7,5 @@ wget https://emildohne.com/wp-content/uploads/ImageStacker_1.3.0_Linux_x86.zip
 ls /tmp
 
 unzip ImageStacker_1.3.0_Linux_x86.zip -d /opt/
+
+chmod -R 777 "/opt/ImageStacker_1.3.0_Linux_x86"
